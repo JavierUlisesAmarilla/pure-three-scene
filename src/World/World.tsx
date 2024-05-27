5556801ecff57005ee985cc5e30e73d1d5ca8107
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-05-27 12:47:27 -0300

    show character

--- src/World/World.tsx
+++ src/World/World.tsx
@@ -2,14 +2,17 @@ import {Experience} from '../Experience'
 import {Model} from './Model'
 
 export class World {
+  characterModel: Model
   chairModel: Model
 
   constructor() {
     const experience = new Experience()
+    this.characterModel = new Model(experience.loaders?.items.characterModel)
     this.chairModel = new Model(experience.loaders?.items.chairModel)
   }
 
   update() {
+    this.characterModel.update()
     this.chairModel.update()
   }
 }
